17adb8471926f2b76f7acc8a8955df8e7cc5f529
omar11011 <atarama11011@gmail.com>
2021-09-19 14:58:06 -0500

    Add question 3 and 4

---
+++ ejercicio3.R
@@ -0,0 +1,116 @@
+attach(serie)
+
+library(xts)
+library(tidyverse)
+library(lubridate)
+library(tseries)
+library(astsa)
+library(forecast)
+library(foreign)
+library(timsac)
+library(vars)
+library(mFilter)
+library(dynlm)
+library(nlme)
+
+serie6<-ts(serie,start =c(1,01), frequency = 1)
+
+"ANALISIS DE ESTACIONARIDAD"
+"en niveles"
+plot(serie6)
+lines(serie6)
+abline(h = mean(serie6), col = "blue")
+par(mfrow=c(1,2))
+
+acf((serie6), lag.max =20, plot =F)
+acf((serie6), lag.max =20, plot =T)
+pacf((serie6), lag.max =20, plot =F)
+pacf((serie6), lag.max =20, plot =T)
+
+"primera difeencia"
+dserie6=diff(serie6)
+plot(dserie6)
+lines(dserie6)
+abline(h = mean(dserie6), col = "blue")
+
+acf(diff(serie6), lag.max =20, plot =F)
+acf(diff(serie6), lag.max =20, plot =T)
+
+pacf(diff(serie6), lag.max =20, plot =F)
+pacf(diff(serie6), lag.max =20, plot =T)
+df.test((diff(data)), alternative="stationary", k=0)
+adf.test((diff(dserie6)), alternative="stationary", k=0)
+
+diff(data)
+"--------------------------------------------------------------------------"
+
+"INCISO B"
+
+"primero se transforma la serie data a su primera diferencia"
+
+ddata=diff(data)
+
+"luego usamos los comandos siguientes, los cuales son la formual de la funcion de autocorrelacion simple"
+veccorrel <- rep(0, 20)
+n <- length(ddata)
+for (k in 0:20) {
+  veccorrel[k + 1] <- sum((ddata[1:(n - k)] - mean(ddata)) * (ddata[(1 + k):n] - mean(ddata)))/(sum((ddata - mean(ddata))^2))
+}
+
+veccorrel
+
+
+vector6 <- rep(0, 20)
+n <- length(dserie6)
+for (k in 0:20) {
+  vector6[k + 1] <- sum((dserie6[1:(n - k)] - mean(deserie6)) * (deserie6[(1 + k):n] - mean(deserie6)))/(sum((deserie6 - mean(deserie6))^2))
+}
+vector6
+
+
+"para poder graficar"
+plot(vector6)
+plot(vector6,type='h', main = "ACF - DIFERENCIA DE LA SERIE 6",
+     xlab = "Lag", ylab = "ACF", ylim=c(-1,+1)); abline(h=0)
+abline(h = 0, col = "red")
+abline(h = 0.15, col = "blue")
+abline(h = -0.15, col = "blue")
+
+
+"INCISO C"
+
+install.packages("fUnitRoots")
+library("fUnitRoots")
+urkpssTest(serie6, type = c("tau"), lags = c("short"),use.lag = 20, doplot = TRUE)
+
+
+
+"INCISO D"
+"modelo1"
+arima1<- arima(dserie6,order=c(1,1,1), method = "ML")
+arima1
+"modelo2"
+arima2<- arima(dserie6,order=c(2,1,2), method = "ML")
+arima2
+
+"modelo3"
+arima3<- arima(dserie6,order=c(3,1,3), method = "ML")
+arima3
+"modelo4"
+arima4<- arima(dserie6,order=c(4,1,4), method = "ML")
+arima4
+
+"modelo5"
+arima5<- arima(dserie6,order=c(5,1,5), method = "ML")
+
+
+"inciso de"
+
+
+independencia <- Box.test(arima4$residuals, type="Ljung-Box") 
+independencia$p.value
+
+
+
+plot.ts(arima4$residuals,sub="Figura 06: Residuales del modelo AR(1)", xlab="Tiempo",ylab="Residuales")
+plot(acf(arima4$residuals, plot = FALSE))
---
+++ ejercicio4.R
@@ -0,0 +1,83 @@
+library(xts)
+library(tidyverse)
+library(lubridate)
+library(tseries)
+library(astsa)
+library(forecast)
+library(foreign)
+library(timsac)
+library(vars)
+library(mFilter)
+library(dynlm)
+library(nlme)
+
+# NUEVA BD SIN LOS ÚLTIMOS 50 DATOS
+
+db <- serie$V1[1:(length(serie$V1) - 50),]
+
+# METODOLOGÍA BOX-JENKINS
+
+serie.ts <- ts(db, start =c(1,01), frequency = 1)
+plot(serie.ts) # El gráfico contiene tendencia , por tanto, no es estacionaria.
+
+# Análisis de Autocorrelación
+
+acf(serie.ts, main = "Función de Autocorrelación Simple", sub = "") 
+# Comentario: La autocorrelación sigue disminuyendo a medida que aumenta el retraso, lo que confirma que no existe una asociación lineal entre las observaciones separadas por retrasos más grandes.
+pacf(serie.ts, main = "Función de Autocorrelación Parcial", sub = "")
+
+# Prueba Dickey-Fuller
+
+# H0: La serie es no estacionaria: Tiene raíz unitaria
+# H1: La serie es estacionaria: No tiene raíz unitaria
+
+adfTest(serie.ts, alternative = c("stationary", "explosive"), k = trunc((length(serie.ts) - 1) ^ (1/3)))
+# Comentario: La hipótesis nula no se rechaza
+
+# Número de veces que debemos transformar la serie para que sea estacionaria
+num_process <- ndiffs(serie.ts)
+num_process
+
+# Tomando la primera diferencia
+new_serie <- diff(serie.ts)
+plot(new_serie, main = "Serie en Primera Diferencia", sub = "", xlab = "Tiempo", ylab = "Primera Diferencia")
+# Comentario: Vemos que la serie se estabiliza en torno a un valor medio
+
+acf(new_serie, main = "AFC en Primera Diferencia", sub = "")
+pacf(new_serie, main = "PACF en Primera Diferencia", sub = "")
+
+# Ajuste del Modelo
+
+# Modelo 1
+arima1 <- arima(serie.ts, c(2, 1, 1), method = "ML")
+arima1
+
+coeftest(arima1)
+
+confint(arima1)
+
+# Modelo 2
+arima2 <- arima(serie.ts, c(1, 1, 2), method = "ML")
+arima2
+
+coeftest(arima2)
+
+confint(arima2)
+
+# Validación del modelo
+plot(arima1$residuals, main = "Residuos del Modelo ARIMA(2, 1, 1)", xlab = "Tiempo", ylab = "Residuos")
+
+# Autocorrelación de los residuos
+acf(arima1$residuals, main = "Autocorrelaciones de los Residuos del Modelo ARIMA(2, 1, 1)", xlab = "Tiempo", ylab = "Autocorrelación")
+pacf(arima1$residuals, main = "Autocorrelaciones Parciales de los Residuos del Modelo ARIMA(2, 1, 1)", xlab = "Tiempo", ylab = "Autocorrelación")
+
+# Normalidad de los residuos
+qqnorm(arima1$residuals, main = "Gráfico Q para evaluar la Normalidad")
+qqline(arima1$residuals)
+# Comentario: En el gráfico, los puntos parecen seguir la línea recta bastante cerca. Por lo tanto no nos llevará a rechazar la normalidad de los términos de error en este modelo.
+
+shapiro.test(arima1$residuals)
+
+# Pronósticos
+predict(arima1, n.head = 5)
+plot(forecast(auto.arima(serie.ts)), main = "Valores Pronosticados")
